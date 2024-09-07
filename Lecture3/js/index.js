a496e3c860c313cf34d3ac51bdd100752ca44694
Ma'aaz Arshad Shaheen <marshad@stellic.com>
2024-09-07 18:48:43 +0500

    added lecture 3

---
+++ Lecture3/js/index.js
@@ -0,0 +1,33 @@
+// var firstName; // console.log("hello external js")
+// firstName = "Maaaz";
+
+let lastName = "Shaheen";
+const age = 24; 
+
+
+lastName = "Arshad"
+console.log(lastName)
+
+if (age == 99) {
+    console.log("wow, you're old");
+
+};
+
+function multiplyByTwo (number) {
+    return number * 2;
+}
+const num = multiplyByTwo(8)
+console.log(num)
+
+const bioData = {
+    "name": "Ma'aaz",
+    "age": 24,
+}
+
+
+
+// console.log(lastName)
+
+
+
+// console.log(age)
